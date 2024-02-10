3e3eda4fe347c5f44683682e85aa1f604ebf0ca1
Amirmohammad <amii_mhmd@yahoo.com>
2024-02-10 11:53:19 +0330

    adding class for mobile view

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from 'react';
+import {FaBars, FaTimes} from 'react-icons/fa';
+import logo from '../assests/logo1.png';
 
 const navbar = () => {
   return (
-    <div className='w-full h-[80px] fixed flex justify-between items-center bg-[#0a192f] text-gray-300'>
+    
+    <div className='w-full h-[80px] fixed flex justify-between items-center px-5 bg-[#0a192f] text-gray-300'>
+      <img src={logo} alt="" style={{width:'50px'}}/>
 
+      <div>
+      <ul className='flex'>
+        <li>Home</li>
+        <li>About</li>
+        <li>Skill</li>
+        <li>Works</li>
+        <li>Exprience</li>
+      </ul>
+    </div>
+
+    {/* // Hambergur */}
+    <div className='hidden'>
+      <FaBars/>
+    </div>
+
+    {/* //mobile view */}
+    <ul className='hidden'>
+        <li>Home</li>
+        <li>About</li>
+        <li>Skill</li>
+        <li>Works</li>
+        <li>Exprience</li>
+    </ul>
+    
     </div>
   )
 }
