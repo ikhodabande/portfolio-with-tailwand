e932cd5f9bd09e9a2a4505b31267783c72e16dfa
Amirmohammad <amii_mhmd@yahoo.com>
2024-02-12 15:11:12 +0330

    adding styles for icons

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import {FaBars, FaTimes, FaLinkedin, FaGithub} from 'react-icons/fa';
-import {HiOutLineMail} from 'react-icons/hi';
+import {HiOutlineMail} from 'react-icons/hi';
 import {BsFillPersonLinesFill} from 'react-icons/bs';
 import logo from '../assests/logo1.png';
 
@@ -40,13 +40,33 @@ const Navbar = () => {
     {/* social icons */}
     <div className='flex flex-col fixed top-[35%] left-0 '>
       <ul>
-        <li className='w-[16px] h-[60px] flex justify-between items-center '> 
-          <a className='flex items-center justify-between  ' 
+        <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#0077b5] '> 
+          <a className='flex  justify-between items-center w-full text-gray-300  ' 
           href="/">
           Linkedin 
+          <FaLinkedin size={30}/>
+          </a>
+        </li>  
+        <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333] '> 
+          <a className='flex  justify-between items-center w-full text-gray-300  ' 
+          href="/">
+          Github
           <FaGithub size={30}/>
           </a>
-
+        </li>  
+        <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#410093] '> 
+          <a className='flex  justify-between items-center w-full text-gray-300  ' 
+          href="/">
+          Email 
+          <HiOutlineMail size={30}/>
+          </a>
+        </li>  
+        <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69] '> 
+          <a className='flex  justify-between items-center w-full text-gray-300  ' 
+          href="/">
+          Resume 
+          <BsFillPersonLinesFill size={30}/>
+          </a>
         </li>  
       </ul>
 
