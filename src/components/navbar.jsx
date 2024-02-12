1e618e570bc16570323adb1969a3491a68da3899
Amirmohammad <amii_mhmd@yahoo.com>
2024-02-12 15:15:39 +0330

    hidden in md and sm

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -38,7 +38,7 @@ const Navbar = () => {
     </ul>
 
     {/* social icons */}
-    <div className='flex flex-col fixed top-[35%] left-0 '>
+    <div className='hidden lg:flex flex-col fixed top-[35%] left-0 '>
       <ul>
         <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#0077b5] '> 
           <a className='flex  justify-between items-center w-full text-gray-300  ' 
