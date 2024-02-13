323607bbef0bd2ce14ab0d4c5c650ca2d8a5b0d5
Amirmohammad <amii_mhmd@yahoo.com>
2024-02-13 16:07:51 +0330

    adding about to app.jsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from './components/navbar';
 import Home from './components/Home';
+import About from './components/About';
 
 function App() {
   return (
     <div>
       <Navbar/>
       <Home/>
+      <About/>
     </div>
   )
 }
