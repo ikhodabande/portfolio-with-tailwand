7f8d6a6f016aaa3234c9445afc53a7507634791e
Amirmohammad <amii_mhmd@yahoo.com>
2024-02-15 12:05:57 +0330

    finish off the skill.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -13,16 +13,44 @@ import Mongo from '../assests/mongo.png';
 
 const Skills = () => {
   return (
-    <div name='skills' className='bg-[#0a192f]'>
+    <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300 '>
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div>
-          <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Expreience</p>
-          <p>// These are the technologies I've worked with</p>
+          <p className='text-4xl font-bold inline border-b-4 border-pink-600 '>Expreience</p>
+          <p className='py-4'>// These are the technologies I've worked with</p>
         </div>
-        <div>
-          <div>
-            <img className='w-20 mx-auto' src="HTML" alt="" />
-            <p>HTML</p>
+        <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={HTML}  />
+            <p className='my-4'>HTML</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={CSS}  />
+            <p className='my-4'>HTML</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={JavaScript}  />
+            <p className='my-4'>HTML</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={ReactImg}  />
+            <p className='my-4'>HTML</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={Node}  />
+            <p className='my-4'>HTML</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={Firbase}  />
+            <p className='my-4'>HTML</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={Tailwind}  />
+            <p className='my-4'>HTML</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={Github}  />
+            <p className='my-4'>HTML</p>
           </div>
         </div>
       </div>
