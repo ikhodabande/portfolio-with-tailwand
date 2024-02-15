05c2f6f98d03c5e1f3d0b4f2345add4d3d2db883
Amirmohammad <amii_mhmd@yahoo.com>
2024-02-15 11:07:04 +0330

    added styles to skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -13,15 +13,15 @@ import Mongo from '../assests/mongo.png';
 
 const Skills = () => {
   return (
-    <div name='skills'>
-      <div>
+    <div name='skills' className='bg-[#0a192f]'>
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div>
-          <p>Expreience</p>
+          <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Expreience</p>
           <p>// These are the technologies I've worked with</p>
         </div>
         <div>
           <div>
-            <img src="HTML" alt="" />
+            <img className='w-20 mx-auto' src="HTML" alt="" />
             <p>HTML</p>
           </div>
         </div>
