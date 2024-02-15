7720bc4e07995b11debb57734c27e0db06e77770
Amirmohammad <amii_mhmd@yahoo.com>
2024-02-15 12:14:59 +0330

    added divs of work.jsx

---
+++ src/components/Work.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Work = () => {
+  return (
+    <div name='work' className='w-full md:h-screen bg-[#0a192f]'>
+      <div>
+      <div>
+        <p>Works</p>
+        <p>//Check out Some Of My Recent Works</p>
+      </div>
+      <div>
+        <div>
+          <div>
+            <div>
+              <span>
+
+              </span>
+              <div>
+                <a href=""><button></button></a>
+                <a href=""><button></button></a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+      </div>
+    </div>
+  )
+}
+
+export default Work
