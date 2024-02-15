3a699885dcde18079d052ba8d006676e1d8f75b7
Amirmohammad <amii_mhmd@yahoo.com>
2024-02-15 10:28:57 +0330

    import imgs

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,4 +1,15 @@
-import React from 'react'
+import React from 'react';
+
+import HTML from '../assests/html.png';
+import CSS from '../assests/css (1).png';
+import JavaScript from '../assests/javascript.png';
+import ReactImg from '../assests/react.png';
+import Node from '../assests/node.png';
+import Firbase from '../assests/firebase (1).png';
+import AWS from '../assests/aws (1).png';
+import Github from '../assests/github.png';
+import Tailwind from '../assests/tailwind.png';
+import Mongo from '../assests/mongo.png';
 
 const Skills = () => {
   return (
