7d60dbf43f46d3af2f73477736dfb8648955b8b7
Amirmohammad <amii_mhmd@yahoo.com>
2024-02-21 00:39:24 +0330

    adding styles to navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
 
   return (
     
-    <div name='home' className='w-full h-[80px] fixed flex justify-between items-center px-5 bg-[#0a192f] text-gray-300'>
+    <div name='navbar' className='w-full h-[80px] fixed flex justify-between items-center px-5 bg-[#0a192f] text-gray-300'>
       <img src={logo} alt="" style={{width:'50px'}}/>
 
       
@@ -44,11 +44,21 @@ const Navbar = () => {
 
     {/* //mobile view */}
     <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'} >
-        <li className='py-6 text-4xl'>Home</li>
-        <li className='py-6 text-4xl'>About</li>
-        <li className='py-6 text-4xl'>Skill</li>
-        <li className='py-6 text-4xl'>Works</li>
-        <li className='py-6 text-4xl'>Exprience</li>
+        <li className='py-6 text-4xl'><Link onClick={handleNav} to="home" smooth={true} duration={500} >
+      Home
+      </Link></li>
+        <li className='py-6 text-4xl'><Link onClick={handleNav} to="about" smooth={true} duration={500} >
+      About
+      </Link></li>
+        <li className='py-6 text-4xl'><Link onClick={handleNav} to="skills" smooth={true} duration={500} >
+      Skills
+      </Link></li>
+        <li className='py-6 text-4xl'><Link onClick={handleNav} to="work" smooth={true} duration={500} >
+      Work
+      </Link></li>
+        <li className='py-6 text-4xl'><Link onClick={handleNav} to="contact" smooth={true} duration={500} >
+      Contact
+      </Link></li>
     </ul>
 
     {/* social icons */}
