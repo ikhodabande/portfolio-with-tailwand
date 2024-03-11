2334b8cbab6da70fe93e72c33a3c84388b4176fc
Amirmohammad <amii_mhmd@yahoo.com>
2024-03-11 18:59:43 +0330

    adding footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from './components/About';
 import Skills from './components/Skills';
 import Work from './components/Work';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <Skills/>
       <Work/>
       <Contact/>
+      <Footer/>
     </div>
   )
 }
