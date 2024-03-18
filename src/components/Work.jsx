fb3f5b6b0c70f9dc281695779b20b7ec6e71614a
Amirmohammad <amii_mhmd@yahoo.com>
2024-03-18 17:38:23 +0330

    import netflix image

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -3,7 +3,7 @@ import workImg from '../assests/project/rent.png';
 import userProject from '../assests/project/user.png';
 import shop from '../assests/project/shop.png';
 import weather from '../assests/project/weather.png';
-import portfolio from '../assests/project/portfolio.png';
+import netflix from '../assests/project/netflix.png';
 
 
 const Work = () => {
@@ -69,7 +69,7 @@ const Work = () => {
            </div>
          </div>
      </div>
-     <div style={{backgroundImage:`url(${portfolio})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'> 
+     <div style={{backgroundImage:`url(${netflix})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'> 
        
        {/* Hover effects */}
          <div className='opacity-0 group-hover:opacity-100'>
